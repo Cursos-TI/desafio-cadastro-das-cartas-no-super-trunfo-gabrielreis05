06e7d15798305cc0003ddac0388dd80417639f85
Gabriel <ghreis2005@gmail.com>
2025-12-05 23:36:56 +0000

    projeto final

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,15 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define linha 10
+#define coluna 11
+
+ int criarCone (int cone[5][5]){
+        int centro = 2;
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j <5; j++){
+                if (j >= centro - i && j <= centro + i && i < 3){
+                    cone[i][j] = 1;
+                } else {
+                    cone[i][j] = 0;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int criarCruz (int cruz[5][5]){
+        int centro = 2;
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j <5; j++){
+                if (i == centro || j == centro ){
+                    cruz[i][j] = 1;
+                } else {
+                    cruz[i][j] = 0;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int criarLosango (int losango[5][5]){
+        int centro = 2;
+        int raio = 2;
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j <5; j++){
+                if (abs(i - centro) + abs(j - centro) <= raio){
+                    losango[i][j] = 1;
+                } else {
+                    losango[i][j] = 0;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int usarHabilidade(int habilidade[5][5], int tabuleiro[linha][coluna], int origemX, int origemY){
+
+    int centro = 2;    
+    for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                if (habilidade[i][j] == 1){
+                    int tabLinha = origemX + (i - centro);
+                    int tabColuna = origemY + (j - centro);
+                }
+            }
+        }
+    }   
 
 int main(){
 
-    int linha = 10, coluna = 11;
     int tabuleiro [linha][coluna];
     char colunaLetra [11] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     int tamanhonavio = 3;
 
-    for (int coluna = 0; coluna < 11; coluna++) {
-        printf("  %c", colunaLetra[coluna]);
+    for (int i = 0; i < coluna; i++) {
+        printf("  %c", colunaLetra[i]);
     }
 
     printf(" \n");
@@ -47,6 +106,19 @@ int main(){
     for (int i = 0; i < tamanhonavio; i++){
         tabuleiro[navio5_linha_inicio + i][navio5_coluna - i] = 3;
     }
+
+    int matrizCone[5][5];
+    criarCone(matrizCone);
+    usarHabilidade(matrizCone, tabuleiro, 5 ,5);
+
+    int matrizCruz[5][5];
+    criarCruz(matrizCruz);
+    usarHabilidade(matrizCruz, tabuleiro, 2 ,7);
+
+    int matrizLosango[5][5];
+    criarLosango(matrizLosango);
+    usarHabilidade(matrizLosango, tabuleiro, 8 ,3);
+
 
     for (int l = 0; l < linha; l++){
         for (int c = 0; c < coluna; c++){
